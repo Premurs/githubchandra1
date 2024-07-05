6765af8164a91c9f313562b14de4594e7f9c72da
Premurs <146364567+Premurs@users.noreply.github.com>
2024-07-05 20:16:23 +0530

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,6 @@
+import time
+print(time.strftime('%H:%M'))
+alarm_time=input('enter the alarm time (HH:MM):')
+while timee.strftime('%H:%M') !=alarm_time:
+    time.sleep(1)
+print('time to wake up!')
